4365c91d67ecb7991b2ee05fa63d4665490973f5
MarioFrancoJ <marioffj@gmail.com>
2026-09-15 14:38:13 -0500

    Change title template format in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
   title: {
     default: SITE_TITLE,
-    template: "%s — Movive",
+    template: "%s | Movive",
   },
   description: SITE_DESCRIPTION,
   applicationName: SITE_NAME,
